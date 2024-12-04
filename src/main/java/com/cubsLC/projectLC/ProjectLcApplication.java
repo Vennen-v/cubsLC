7b982bd196e246347ce05c8ebcede6d2bf2affb7
Jeffery Phipps <jefferyphipps@att.net>
2024-12-04 01:32:31 -0600

    added spring boot files to start project

---
+++ src/main/java/com/cubsLC/projectLC/ProjectLcApplication.java
@@ -0,0 +1,13 @@
+package com.cubsLC.projectLC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubsLC/projectLC/ProjectLcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubsLC.projectLC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
